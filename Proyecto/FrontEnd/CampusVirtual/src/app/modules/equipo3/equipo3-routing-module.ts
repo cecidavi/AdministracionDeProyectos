9352a9f36d6d5307c89545e349fdebf22cced512
cecidavi <cecireicito_@hotmail.com>
2025-11-17 16:09:27 -0600

    Sofi

--- Proyecto/FrontEnd/CampusVirtual/src/app/modules/equipo3/equipo3-routing-module.ts
+++ Proyecto/FrontEnd/CampusVirtual/src/app/modules/equipo3/equipo3-routing-module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HolaEquipo3 } from './src/hola-equipo3/hola-equipo3';
+import { Deportes } from './src/deportes/deportes';
+import { Calendario } from './src/deportes/calendario/calendario';
 
 const routes: Routes = [
     {
         path: '',
         component: HolaEquipo3,
-    }
+    },
+      {
+    path: 'deportes',
+    component: Deportes
+  },
+  {
+    path: 'deportes/calendario',
+    component: Calendario
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class Equipo3RoutingModule { }
+export class Equipo3RoutingModule {}
--- Proyecto/FrontEnd/CampusVirtual/src/app/modules/equipo3/src/deportes/calendario/calendario.ts
+++ Proyecto/FrontEnd/CampusVirtual/src/app/modules/equipo3/src/deportes/calendario/calendario.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './calendario.css'
 })
 export class Calendario {
-
+partidos = [
+    {
+      fecha: '2025-11-20',
+      hora: '14:00',
+      equipoA: 'Tigres',
+      equipoB: 'Leones'
+    },
+    {
+      fecha: '2025-11-22',
+      hora: '16:30',
+      equipoA: 'Pumas',
+      equipoB: 'Águilas'
+    },
+    {
+      fecha: '2025-11-25',
+      hora: '12:00',
+      equipoA: 'Halcones',
+      equipoB: 'Toros'
+    }
+  ];
 }
